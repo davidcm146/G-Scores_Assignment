70b8ad73747986d5205d2b917f9d24b7b314e1c0
21120472 <quachhuycm@gmail.com>
2024-11-22 23:42:14 +0700

    fix : fix cors to allow all

--- Backend/index.js
+++ Backend/index.js
@@ -8,18 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 
 const corsOptions = {
-    origin: function (origin, callback) {
-      const allowedOrigins = [process.env.FRONTEND_URL];
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  };
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
 
 app.use(cors(corsOptions));
 
