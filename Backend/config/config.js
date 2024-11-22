a2b4863dfb1b05703dbf228fca1e86b3cfa468df
21120472 <quachhuycm@gmail.com>
2024-11-22 15:34:15 +0700

    change : change property in production in config

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DBNAME,
+    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
   }
 };
