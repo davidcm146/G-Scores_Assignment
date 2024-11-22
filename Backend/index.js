9839d1ab3e5dd78a840d7cb7d667176b3719d634
21120472 <quachhuycm@gmail.com>
2024-11-23 00:08:03 +0700

    fix : config vercel 2

--- Backend/index.js
+++ Backend/index.js
@@ -17,4 +17,5 @@ app.use(cors(corsOptions));
 
 app.use("/api", router);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server running on port 3000"));
