9d392bcb50f8a6ce84e029a74ce1f9cfe8e8949c
21120472 <quachhuycm@gmail.com>
2024-11-22 22:47:26 +0700

    fix : fix cors options

--- Backend/index.js
+++ Backend/index.js
@@ -7,12 +7,19 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-// Configure CORS for the specific domain
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
+    origin: function (origin, callback) {
+      const allowedOrigins = [process.env.FRONTEND_URL];
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  };
 
 app.use(cors(corsOptions));
 
