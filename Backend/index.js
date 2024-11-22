da6d32aee6d3eca2864c3034f8dd935af8b4f15d
21120472 <quachhuycm@gmail.com>
2024-11-22 22:43:05 +0700

    add : add vercel json file

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const router = require("./routes/api");
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
